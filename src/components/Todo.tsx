c06a4e96b74f8fca162a48e954c063ed47cf3260
William Blackwell <wbprogramming@icloud.com>
2024-02-06 17:30:44 -0500

    added todo enviroments where each env has its own todo list

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,38 +1,33 @@
-import React, { useState } from 'react';
+
+
+import React, { useState, useEffect } from 'react';
 import './ToDo.scss';
 import 'material-icons/iconfont/material-icons.scss';
 
 interface ToDoProps {
-    toDos: {
-        id: number;
-        text: string;
-        completed: boolean;
-        editMode: boolean;
-    }[];
+    title: string;
+    environment: string;
 }
 
-const ToDo: React.FC<ToDoProps> = ({ toDos }) => {
+const ToDo: React.FC<ToDoProps> = ({ title, environment }) => {
     const [newTodo, setNewTodo] = useState<string>('');
-    const [todos, setTodos] = useState(toDos);
-    const [editTodos, setEditTodos] = useState<{ [key: number]: string }>({});
+    const [editTodo, setEditTodo] = useState<{ id: number, text: string } | null>(null);
+    const [todos, setTodos] = useState<{ id: number, text: string, completed: boolean, editMode: boolean, environment: string }[]>([]);
 
-     // Load todos from localStorage on component mount
-     React.useEffect(() => {
+    useEffect(() => {
         const savedTodos = localStorage.getItem('todos');
         if (savedTodos) {
             setTodos(JSON.parse(savedTodos));
         }
     }, []);
 
-    // Save todos to localStorage whenever todos change
-    React.useEffect(() => {
+    useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
 
-
     const handleAddTodo = () => {
         if (newTodo.trim() !== '') {
-            const newToDo = { id: todos.length, text: newTodo, completed: false, editMode: false };
+            const newToDo = { id: todos.length, text: newTodo, completed: false, editMode: false, environment };
             setTodos([...todos, newToDo]);
             setNewTodo('');
         }
@@ -51,61 +46,71 @@ const ToDo: React.FC<ToDoProps> = ({ toDos }) => {
     }
 
     const handleEditTodo = (id: number) => {
-        const updatedEditTodos = { ...editTodos };
-        updatedEditTodos[id] = todos.find(todo => todo.id === id)?.text || '';
-        setEditTodos(updatedEditTodos);
-
-        const updatedToDos = todos.map(todo =>
-            todo.id === id ? { ...todo, editMode: true } : todo
-        );
-        setTodos(updatedToDos);
+        const todoToEdit = todos.find(todo => todo.id === id);
+        if (todoToEdit) {
+            setEditTodo({ id: todoToEdit.id, text: todoToEdit.text });
+            const updatedToDos = todos.map(todo =>
+                todo.id === id ? { ...todo, editMode: true } : todo
+            );
+            setTodos(updatedToDos);
+        }
     }
 
-    const handleSaveEdit = (id: number) => {
-        const updatedToDos = todos.map(todo => {
-            if (todo.id === id) {
-                return { ...todo, text: editTodos[id], editMode: false };
-            }
-            return todo;
-        });
-        setTodos(updatedToDos);
+    const handleSaveEdit = (editTodo: { id: number, text: string }) => {
+        if (editTodo) {
+            const updatedToDos = todos.map(todo => {
+                if (todo.id === editTodo.id) {
+                    return { ...todo, text: editTodo.text, editMode: false };
+                }
+                return todo;
+            });
+            setTodos(updatedToDos);
+            setEditTodo(null); 
+        }
     }
 
     return (
         <div className="ToDo">
+            <h1 className='ToDo__Location'>{title}</h1>
             <h1 className='ToDo__Title'>ToDooze</h1>
             <form className="ToDo__Form">
                 <input placeholder='TaDooze' className="ToDo__Input" type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
                 <button className="ToDo__Button" type="button" onClick={handleAddTodo}>Add</button>
             </form>
             <ul className="ToDo__List">
-                {todos.map((todo) => (
-                    <div className='ToDo__Container' key={todo.id}>
-                        {editTodos[todo.id] && todo.editMode && <span onClick={() => handleSaveEdit(todo.id)} className="material-icons-outlined">save</span>}
-                        {!todo.editMode && <span onClick={() => handleEditTodo(todo.id)} className="material-icons-outlined">edit</span>}
-                        {todo.editMode ?
-                            <input
-                                placeholder='Edit Todo'
-                                className="ToDo__Item"
-                                type="text"
-                                value={editTodos[todo.id]}
-                                onChange={(e) => setEditTodos({ ...editTodos, [todo.id]: e.target.value })}
-                            /> :
-                            <li
-                                style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                                onClick={() => handleCompletedToDo(todo.id)}
-                                className="ToDo__Item"
-                            >
-                                {todo.text}
-                            </li>
-                        }
-                        <span onClick={() => handleDeleteTodo(todo.id)} className="material-icons-outlined">delete</span>
-                    </div>
-                ))}
+                {todos.map((todo) => {
+                    if (todo.environment === environment) {
+                        return (
+                            <div className='ToDo__Container' key={todo.id}>
+                                {editTodo && editTodo.id === todo.id && todo.editMode && <span onClick={() => handleSaveEdit(editTodo)} className="material-icons-outlined">save</span>}
+
+                                {!todo.editMode && <span onClick={() => handleEditTodo(todo.id)} className="material-icons-outlined">edit</span>}
+                                {todo.editMode ?
+                                    <input
+                                        placeholder='Edit Todo'
+                                        className="ToDo__Item"
+                                        type="text"
+                                        value={editTodo?.text || ''}
+                                        onChange={(e) => setEditTodo(prevState => ({ ...(prevState || {}), text: e.target.value, id: prevState?.id || 0 }))}
+                                    /> :
+                                    <li
+                                        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+                                        onClick={() => handleCompletedToDo(todo.id)}
+                                        className="ToDo__Item"
+                                    >
+                                        {todo.text}
+                                    </li>
+                                }
+                                <span onClick={() => handleDeleteTodo(todo.id)} className="material-icons-outlined">delete</span>
+                            </div>
+                        );
+                    }
+                    return null;
+                })}
             </ul>
-            {todos.length === 0 && <><span className="material-icons-outlined">add_task</span> <span className="ToDo__Empty">You have nothing ToDooze</span></>}
+            {todos.length === 0 && <><span className="material-icons-outlined">add_task</span> <span className='ToDo__Empty'>Congrats, you have nothing ToDooze</span></>}
             {todos.length > 0 && <button onClick={() => setTodos([])} className="ToDo__Clear" type="button">Clear List</button>}
-        <div className='ToDo__Footer'>Created by: Will-i-am</div>
+            <div className='ToDo__Footer'>Created by: Will-i-am</div>
         </div>
     );
 };
